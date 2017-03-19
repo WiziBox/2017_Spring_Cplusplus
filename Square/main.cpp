e7e06133810c8eb217b0a3c5e695be738a757e2d
이상현 <thslr2020@gmail.com>
2017-03-19 15:33:03 +0900

    Initial Commit

---
+++ Square/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Square
+//
+//  Created by MISTY on 2017. 3. 19..
+//  Copyright © 2017년 MISTY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
