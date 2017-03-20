55dcb62ee941c2b3f2cfb53f60d07c74a2b61601
이상현 <thslr2020@gmail.com>
2017-03-21 00:36:07 +0900

    First Commit

--- Square/main.cpp
+++ Square/main.cpp
@@ -8,8 +8,51 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int width, height;/* area, job;*/
+    
+    //	cout << endl << "원하시는 작업을 선택하세요" << endl << "1.가로 길이 입력"<< endl << "2.세로 길이 입력" << "3.사각형 넓이 계산 << endl;
+    //	cin >> job;
+    
+    //	switch (job)
+    //	{
+    //		case(1) :
+    //		{
+    cout << "가로의 길이를 입력해 주세요";
+    cin >> width;
+    //if(width == int);
+    //		int setWidth(width);
+    //else(cout << "잘못된 값을 입력하셨습니다!");
+    //		break;
+    //		}
+    
+    //		case(2) :
+    //		{
+    cout << "세로의 길이를 입력해 주세요";
+    cin >> height;
+    //		int setHeight(height);
+    //		break;
+    //		}
+    
+    //		case(3) :
+    //		{
+    //		int getArea = 0;
+    cout << "사각형의 넓이는 " << width *  height << "입니다." << endl;
+    //		break;
+    //		}
+    //	}
     return 0;
+    
+    /*class square
+     {
+     private:
+     int width;
+     int height;
+     public:
+     void setWidth();
+     void setHeight();
+     }*/
 }
