051b100258d5d09625e0a9fc3b1f679cb5164148
이상현 <thslr2020@gmail.com>
2017-03-22 03:04:58 +0900

    2017. 03 .22 - Almost completed?

--- Square/main.cpp
+++ Square/main.cpp
@@ -10,49 +10,98 @@
 
 using namespace std;
 
+class Square
+{
+    
+private:
+    double width, height, modWidth, modHeight;
+    
+public:
+    double setWidth(), setHeight(), getArea();
+    int job;
+    
+    void setWidth(int x1)
+    {
+        width = x1;
+    }
+    
+    void setHeight(int y1)
+    {
+        height = y1;
+    }
+    
+    double getArea(int a1, int x1, int y1)
+    {
+        return a1 = x1 * y1;
+    }
+    
+    void modifyWidth(int x2)
+    {
+        modWidth = x2;
+    }
+    
+    void modifyHeight(int y2)
+    {
+        modHeight = y2;
+    }
+    
+};
+
 int main()
 {
-    int width, height;/* area, job;*/
+    double a1, x1, x2 = 0.0, y1, y2 = 0.0;
+    int job;
     
-    //	cout << endl << "원하시는 작업을 선택하세요" << endl << "1.가로 길이 입력"<< endl << "2.세로 길이 입력" << "3.사각형 넓이 계산 << endl;
-    //	cin >> job;
+    cout << "가로의 길이를 입력해 주세요 : ";
+        cin >> x1;
+
+    cout << "세로의 길이를 입력해 주세요 : ";
+        cin >> y1;
     
-    //	switch (job)
-    //	{
-    //		case(1) :
-    //		{
-    cout << "가로의 길이를 입력해 주세요";
-    cin >> width;
-    //if(width == int);
-    //		int setWidth(width);
-    //else(cout << "잘못된 값을 입력하셨습니다!");
-    //		break;
-    //		}
+    cout << "사각형의 넓이는 " << x1 * y1 << "입니다." << endl;
+ 
+    while(1)
+    {
+        cout << endl << "원하시는 작업을 선택하세요 " << "1.가로 길이 수정 " << "2.세로 길이 수정 " << "3.작업 종료" << endl;
+        cin >> job;
     
-    //		case(2) :
-    //		{
-    cout << "세로의 길이를 입력해 주세요";
-    cin >> height;
-    //		int setHeight(height);
-    //		break;
-    //		}
-    
-    //		case(3) :
-    //		{
-    //		int getArea = 0;
-    cout << "사각형의 넓이는 " << width *  height << "입니다." << endl;
-    //		break;
-    //		}
-    //	}
+        if(job==3) break;
+        
+        switch (job)
+        {
+            case(1) :
+            {
+                cout << "가로의 길이를 입력해 주세요 : ";
+                cin >> x2;
+                if(y2==0)
+                {
+                cout << "사각형의 넓이는 " << x2 * y1 << "입니다." << endl;
+                }
+                else
+                {
+                cout << "사각형의 넓이는 " << x2 * y2 << "입니다." << endl;
+                }
+                break;
+            }
+            
+            case(2) :
+            {
+                cout << "세로의 길이를 입력해 주세요 : ";
+                cin >> y2;
+                if(x2==0)
+                {
+                cout << "사각형의 넓이는 " << x1 * y2 << "입니다." << endl;
+                }
+                else
+                {
+                cout << "사각형의 넓이는 " << x2 * y2 << "입니다." << endl;
+                }
+                break;
+            }
+
+            case(3) : { break; }
+            default : cout << "다시 입력해주세요"; break;
+        }
+    }
     return 0;
-    
-    /*class square
-     {
-     private:
-     int width;
-     int height;
-     public:
-     void setWidth();
-     void setHeight();
-     }*/
 }
